fde6ff4fa142c5b2f064fb64515abbe3fd813ee7
nisalmissaka <wishwakanisal@gmail.com>
2026-05-03 19:04:56 +0530

    Implement score and best score display with styled text and layout

--- game_app/lib/Homepage.dart
+++ game_app/lib/Homepage.dart
@@ -65,7 +65,29 @@ class _HomePageState extends State<HomePage> {
           ),
           Expanded(
             child: Container(
-              color: Colors.green,
+              color: Colors.brown,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("SCORE", style: TextStyle(color: Colors.white,fontSize: 20),),
+                    SizedBox(height: 20,),
+                    Text("0",style: TextStyle(color: Colors.white,fontSize: 35)),
+                  ],
+                ),
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("BEST", style: TextStyle(color: Colors.white,fontSize: 20)),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text("10",style: TextStyle(color: Colors.white,fontSize: 35)),
+                  ],
+                )
+              ],),
             ),
           ),
         ],
